24c44c97d201cfeb036041a767d95db6902fefe5
WebSurfinMurf <websurfinmurf@gmail.com>
2025-06-30 20:48:31 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -84,6 +84,7 @@ docker run -d \
   -e KC_DB_USERNAME="${POSTGRES_USER}" \
   -e KC_DB_PASSWORD="${POSTGRES_PASSWORD}" \
   -e KC_HOSTNAME="${PUBLIC_HOSTNAME}" \
+  -e KC_HOSTNAME_ADMIN="${PUBLIC_HOSTNAME}" \
   --label "traefik.enable=true" \
   --label "traefik.http.routers.keycloak-secure.rule=Host(\`${PUBLIC_HOSTNAME}\`)" \
   --label "traefik.http.routers.keycloak-secure.entrypoints=websecure" \
